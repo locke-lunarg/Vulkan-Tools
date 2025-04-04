6e3ffa1ad31ff87410cceab6adb8db7a59c2523a
Locke Lin <locke@lunarg.com>
2025-04-04 17:46:30 -0600

    Update cube.c

--- cube/cube.c
+++ cube/cube.c
@@ -881,16 +881,7 @@ static void demo_set_image_layout(struct demo *demo, VkImage image, VkImageAspec
 
 static void demo_draw_build_cmd(struct demo *demo, VkCommandBuffer cmd_buf) {
 
-    VkCommandBufferInheritanceInfo inheritance_info = {
-        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO,
-        .pNext = NULL,
-        .renderPass = demo->render_pass,
-        .subpass = 0,
-        .framebuffer = demo->swapchain_image_resources[demo->current_buffer].framebuffer,
-        .occlusionQueryEnable = VK_FALSE,
-        .queryFlags = 0,
-        .pipelineStatistics = 0,
-    };
+    VkCommandBufferInheritanceInfo inheritance_info;
 
     const VkCommandBufferBeginInfo cmd_buf_info = {
         .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
@@ -994,16 +985,7 @@ static void demo_draw_build_cmd(struct demo *demo, VkCommandBuffer cmd_buf) {
 void demo_build_image_ownership_cmd(struct demo *demo, int i) {
     VkResult U_ASSERT_ONLY err;
 
-    VkCommandBufferInheritanceInfo inheritance_info = {
-        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO,
-        .pNext = NULL,
-        .renderPass = demo->render_pass,
-        .subpass = 0,
-        .framebuffer = demo->swapchain_image_resources[demo->current_buffer].framebuffer,
-        .occlusionQueryEnable = VK_FALSE,
-        .queryFlags = 0,
-        .pipelineStatistics = 0,
-    };
+    VkCommandBufferInheritanceInfo inheritance_info;
 
     const VkCommandBufferBeginInfo cmd_buf_info = {
         .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
@@ -2427,16 +2409,7 @@ static void demo_prepare(struct demo *demo) {
     assert(!err);
     demo_name_object(demo, VK_OBJECT_TYPE_COMMAND_BUFFER, (uint64_t)demo->cmd, "PrepareCB");
 
-    VkCommandBufferInheritanceInfo inheritance_info = {
-        .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_INHERITANCE_INFO,
-        .pNext = NULL,
-        .renderPass = demo->render_pass,
-        .subpass = 0,
-        .framebuffer = demo->swapchain_image_resources[demo->current_buffer].framebuffer,
-        .occlusionQueryEnable = VK_FALSE,
-        .queryFlags = 0,
-        .pipelineStatistics = 0,
-    };
+    VkCommandBufferInheritanceInfo inheritance_info;
 
     VkCommandBufferBeginInfo cmd_buf_info = {
         .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO,
